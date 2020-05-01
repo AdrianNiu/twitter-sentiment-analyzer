67d9e6852bd6e8d005ee94ee6e9946ec324a4d14
Adrian Niu <niuxx107@umn.edu>
2020-04-30 22:21:08 -0400

    evironment set up

---
+++ src/components/AboutPage/AboutPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// This is one of our simplest components
+// It doesn't have local state, so it can be a function component.
+// It doesn't dispatch any redux actions or display any part of redux state
+// or even care what the redux state is, so it doesn't need 'connect()'
+
+const AboutPage = () => (
+  <div>
+    <div>
+      <p>
+        This about page is for anyone to read!
+
+        Adrian
+      </p>
+    </div>
+  </div>
+);
+
+export default AboutPage;
