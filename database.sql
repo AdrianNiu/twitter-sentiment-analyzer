49e413facce66292138a7d8107c594000ed5ae21
Adrian Niu <niuxx107@umn.edu>
2020-05-20 23:12:47 -0400

    updating database.sql file

--- database.sql
+++ database.sql
@@ -3,8 +3,12 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- input for more table
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+
