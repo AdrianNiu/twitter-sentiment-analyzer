85b2b1d41a3e46a281952ef2d75a36de9e0f6d3e
Adrian Niu <niuxx107@umn.edu>
2020-05-30 23:17:22 -0400

    addedd GET reducer and SAGA for PIES

--- src/redux/sagas/addPieSaga.js
+++ src/redux/sagas/addPieSaga.js
@@ -8,14 +8,27 @@ function* postPieSaga(action) {
         //Making asyn AJAX (axios) request
         yield axios.post(`/api/pie`, action.payload);
         //Request information back from the server after change
-        // yield put({ type: 'GET_FAV' });
+        yield put({ type: 'GET_PIE' });
     } catch (error) {
         console.log('error with put request for adding pies', error);
     }
 }
 
+function* getPieSaga(action) {
+    console.log('in getPieSaga', action);
+    try {
+        const response = yield axios.get('/api/pie');
+        console.log('Heres the GET response for /api/pie', response.data);
+        yield put({ type: 'PIES', payload: response.data })
+    }
+    catch (error) {
+        console.log('Error with PIES GET', error);
+    }
+}
+
 function* addPieSaga() {
     yield takeLatest('SAVE_PIE', postPieSaga);
+    yield takeLatest('GET_PIE', getPieSaga);
 }
 
 export default addPieSaga;
