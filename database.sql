5cac3394bc72a1cb667ce04587c2e4cf57dc3410
Adrian Niu <niuxx107@umn.edu>
2020-06-21 21:28:12 -0400

    updated sql file

--- database.sql
+++ database.sql
@@ -14,3 +14,60 @@ CREATE TABLE "user" (
 );
 
 
+CREATE TABLE "user"
+(
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
+);
+
+
+ALTER TABLE "user"
+ ADD "email" VARCHAR (255);
+
+CREATE TABLE "search_result"
+(
+    "user_id" INT REFERENCES "user",
+    "sentiment_id" INT REFERENCES "sentiment_result"
+);
+
+CREATE TABLE "sentiment_result"
+(
+    "id" SERIAL PRIMARY KEY,
+    "time_stamp" TIMESTAMP,
+    "sentiment_text" VARCHAR (512)
+);
+
+ALTER TABLE "sentiment_result"
+ ADD "sentiment_score" INT;
+
+ALTER TABLE "sentiment_result"
+ ADD "sentiment" VARCHAR(50);
+
+ALTER TABLE "sentiment_result"
+ ADD "time" VARCHAR(100);
+
+ALTER TABLE "sentiment_result" 
+DROP "time_stamp ";
+
+ALTER TABLE "sentiment_result" 
+ADD "keyword" VARCHAR(100);
+
+ALTER TABLE "sentiment_result" 
+ADD "note" VARCHAR(1000);
+
+ALTER SEQUENCE sentiment_result_id_seq RESTART WITH 1;
+
+ALTER TABLE "sentiment_result" 
+ADD "notes" VARCHAR(1000);
+
+CREATE TABLE "sentiment_pie"
+(
+    "id" SERIAL PRIMARY KEY,
+    "pie_negative" NUMERIC,
+    "pie_neutral" NUMERIC,
+    "pie_positive" NUMERIC,
+    "notes" VARCHAR(1000),
+    "keyword" VARCHAR(100),
+    "time" VARCHAR(100)
+);
