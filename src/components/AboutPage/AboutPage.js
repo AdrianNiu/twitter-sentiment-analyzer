959401c24c70c32c862696fe09bb9c2f608e4076
Adrian Niu <niuxx107@umn.edu>
2020-06-29 23:02:02 -0400

    deleted extra code in about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -36,38 +36,6 @@ class AboutPage extends Component {
     })
   };
 
-  // addToFav = (url) => {
-  //   this.props.dispatch({ type: "POST_FAV", payload: { imageurl: url } })
-  // }
-
-  // state = {
-  //   dataPie: {
-  //     labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-  //     datasets: [
-  //       {
-  //         data: [300, 50, 100, 40, 120],
-  //         backgroundColor: [
-  //           "#F7464A",
-  //           "#46BFBD",
-  //           "#FDB45C",
-  //           "#949FB1",
-  //           "#4D5360",
-  //           "#AC64AD"
-  //         ],
-  //         hoverBackgroundColor: [
-  //           "#FF5A5E",
-  //           "#5AD3D1",
-  //           "#FFC870",
-  //           "#A8B3C5",
-  //           "#616774",
-  //           "#DA92DB"
-  //         ]
-  //       }
-  //     ]
-  //   }
-  // }
-
-
 
   render() {
     if (this.state.hasSearched) {
